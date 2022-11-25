22abea25c582ff9cf73065de50dd69a8001dffb5
talabidele <talabibamidele7@gmail.com>
2022-11-25 13:21:20 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from "./pages/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Nav from "./components/Nav";
+import VSM from "./pages/VSM";
+import Hero from "./components/Hero";
+import Landing from "./pages/Landing";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route exact path="/vsm" element={<VSM />} />
+          <Route path="/telemedicine" element={<Home />} />
+          {/* <Route path="/vsm" element={<VSM />} /> */}
+        </Routes>
+      </Router>
     </div>
   );
 }
